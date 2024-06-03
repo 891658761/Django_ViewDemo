76225af7e2987d2b9b16083c491362391a1bba10
枫念 <167508600+891658761@users.noreply.github.com>
2024-06-03 17:17:31 +0800

    Update config.py

--- school_bed/school_bed/config.py
+++ school_bed/school_bed/config.py
@@ -1,5 +1,5 @@
-userName = "Kwl891658761@163.com"
-passWord = "5204901109"
+userName = "用户名"
+passWord = "密码"
 header = {
     'User-Agent': "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36"
 }
